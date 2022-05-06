683f831585948f495814c8f4e3a114dd94b0c95f
İbrahim EFE <ibrahim.efee@hotmail.com>
2022-05-06 20:46:01 +0300

    Mongo db connected

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,12 +1,12 @@
 <?php
 
-// use Illuminate\Database\Migrations\Migration;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Database\Migrations\Migration;
+// use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends DatabaseMigrations
+return new class extends Migration
 {
     /**
      * Run the migrations.
